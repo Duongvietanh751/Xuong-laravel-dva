e09f2b7dbffe35477927e395c723df94c8419be0
Duongvietanh751 <duongvietanh751@gmail.com>
2024-06-02 15:29:54 +0700

    buổi 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
-use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +20,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('categories',CategoryController::class);
+ Auth::routes();
+//Route::get('auth/login', [LoginController::class , 'showFormLogin'])->name('login');
+//Route::post('auth/login', [LoginController::class , 'login']);
+//
+//Route::post('auth/logout', [LoginController::class , 'logout'])->name('logout');
+//
+//Route::get('auth/register', [RegisterController::class , 'showFormRegister'])->name('register');
+//Route::post('auth/register', [RegisterController::class , 'register']);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
